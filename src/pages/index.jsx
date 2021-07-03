e6e59fe0cc6ceb84ea7955d487d5bc0ef4a1bb86
sitek94 <msitkowski94@gmail.com>
2021-07-03 18:08:33 +0200

    refactor: remove old design and leave index page blank

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,62 +1,18 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import '../styles/index.css';
-import { StaticImage } from 'gatsby-plugin-image';
 
-import Navbar from '../components/navbar';
-import { graphql } from 'gatsby';
-import tw from 'twin.macro';
-
-const Button = tw.button`
-  bg-blue-500 hover:bg-blue-800 text-white p-2 rounded
-`;
-
-export default function Home({ data }) {
+export default function Home() {
   return (
     <div>
       <Helmet htmlAttributes={{ lang: 'pl' }}>
-        <title>Wieslaw Transport</title>
+        <title>Wiesław Transport</title>
         <meta charSet="utf-8" />
         <meta
           name="description"
           content="Usługi transportowe, przewóz osób autokarem, busem oraz samochodem osobowym. Wyjazdy krajowe i zagraniczne. Kontakt tel: 601 819 653. Adres email: wieslaw.trans@gmail.com, Matuszewski Wiesław."
         />
       </Helmet>
-      <Navbar title={data.site.siteMetadata.title} />
-      <main id="start">
-        <Button>Activate</Button>
-        <StaticImage
-          className="hero-image"
-          src="../images/hero.jpg"
-          alt="hero"
-          placeholder="blurred"
-          layout="fullWidth"
-        />
-
-        <StaticImage
-          src="../images/inside.jpg"
-          alt="inside"
-          placeholder="blurred"
-          layout="fullWidth"
-        />
-
-        <StaticImage
-          src="../images/distance.jpg"
-          alt="distance"
-          placeholder="blurred"
-          layout="fullWidth"
-        />
-      </main>
     </div>
   );
 }
-
-export const query = graphql`
-  query HomePageQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
