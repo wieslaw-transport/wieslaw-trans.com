b7ab4c5939cd4b0186ab514b8088f46d3ddcebb7
sitek94 <msitkowski94@gmail.com>
2021-07-04 22:43:42 +0200

    feat(navbar): create Navbar as per mockup, closes #6

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import '../styles/index.css';
 
 export default function Home() {
   return (
@@ -13,6 +12,29 @@ export default function Home() {
           content="Usługi transportowe, przewóz osób autokarem, busem oraz samochodem osobowym. Wyjazdy krajowe i zagraniczne. Kontakt tel: 601 819 653. Adres email: wieslaw.trans@gmail.com, Matuszewski Wiesław."
         />
       </Helmet>
+
+      <div id="navigation" className="px-12 py-8">
+        <div className="flex flex-grow relative">
+          <a href="/">
+            <span className="font-display font-black border-b-4 border-gray-900">WiesławTrans</span>
+          </a>
+
+          <nav className="absolute flex w-full justify-center">
+            <a className="font-medium uppercase text-sm px-5 opacity-60 hover:opacity-95" href="/">
+              start
+            </a>
+            <a className="font-medium uppercase text-sm px-5 opacity-60 hover:opacity-95" href="/">
+              o nas
+            </a>
+            <a className="font-medium uppercase text-sm px-5 opacity-60 hover:opacity-95" href="/">
+              usługi
+            </a>
+            <a className="font-medium uppercase text-sm px-5 opacity-60 hover:opacity-95" href="/">
+              kontakt
+            </a>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
