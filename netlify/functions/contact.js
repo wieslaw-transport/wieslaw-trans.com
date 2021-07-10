0dd74d82051fcbfbd087944b4018807e679923ce
sitek94 <msitkowski94@gmail.com>
2021-07-10 23:52:00 +0200

    feat(sendgrid): change 'from' email to the authenticated one

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -4,7 +4,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msg = {
   to: 'msitkowski94@gmail.com', // Change to your recipient
-  from: 'test@example.com', // Change to your verified sender
+  from: 'hello@wieslaw-trans.com', // Change to your verified sender
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
