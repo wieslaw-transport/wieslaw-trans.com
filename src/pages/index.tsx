58e4db7c0ff910eccf7e58d0dc97fb6d4b09c5f8
sitek94 <msitkowski94@gmail.com>
2021-07-10 17:03:44 +0200

    feat(contact): make Contact section responsive on mobiles
    Closes #32

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,8 @@ import { StaticImage } from 'gatsby-plugin-image';
 
 import Navigation from '../components/navigation';
 import AboutUsSection from '../components/about-us';
-import Services from '../components/services';
+import ServicesSection from '../components/services';
+import ContactSection from '../components/contact';
 
 export default function Home() {
   return (
@@ -36,78 +37,13 @@ export default function Home() {
           <StaticImage src="../images/inside.jpg" alt="Wnętrze autokaru marki Mercedes" />
         </section>
 
-        <Services />
+        <ServicesSection />
 
         <section id="distant-photo-section">
           <StaticImage src="../images/distance.jpg" alt="Autokar marki Mercedes na tle kościoła" />
         </section>
 
-        <section id="contact-section" className="w-full max-w-screen-md px-6 py-4 mx-auto">
-          <div className="text-center font-display mb-12">
-            <label className="text-xs font-medium uppercase opacity-60 tracking-widest mb-2.5">Kontakt</label>
-            <h2 className="text-4xl">Skontaktuj się z nami!</h2>
-          </div>
-
-          <div className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3">
-            <a href="#" className="flex flex-col items-center px-4 py-3 text-gray-700 rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
-              <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
-              </svg>
-
-              <span className="mt-2">Piaskowa 7, Pułtusk</span>
-            </a>
-
-            <a href="#" className="flex flex-col items-center px-4 py-3 text-gray-700 rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
-              <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-              </svg>
-
-              <span className="mt-2">601 819 653</span>
-            </a>
-
-            <a href="#" className="flex flex-col items-center px-4 py-3 text-gray-700 rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
-              <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-              </svg>
-
-              <span className="mt-2">wieslaw.trans@gmail.com</span>
-            </a>
-          </div>
-          <div className="mt-6 ">
-            <div className="items-center -mx-2 md:flex">
-              <div className="w-full mx-2">
-                <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">Imię i nazwisko</label>
-
-                <input
-                  className="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                  type="text"
-                />
-              </div>
-
-              <div className="w-full mx-2 mt-4 md:mt-0">
-                <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">E-mail</label>
-
-                <input
-                  className="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                  type="email"
-                />
-              </div>
-            </div>
-
-            <div className="w-full mt-4">
-              <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">Wiadomość</label>
-
-              <textarea className="block w-full h-40 px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"></textarea>
-            </div>
-
-            <div className="flex justify-center mt-6">
-              <button className="px-4 py-2 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-                Wyślij wiadomość
-              </button>
-            </div>
-          </div>
-        </section>
+        <ContactSection />
       </main>
 
       <footer className="p-12">
